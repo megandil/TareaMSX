e4c35b40fac926d57619997e61316be436258334
Daniel Mesa <danimesamejias@gmail.com>
2021-05-14 12:39:39 +0200

    Implementación de /juegos

--- app.py
+++ app.py
@@ -3,4 +3,8 @@ app = Flask(__name__)
 @app.route('/')
 def inicio():
     return render_template("inicio.html")
+
+@app.route('/juegos')
+def juegos():
+    return render_template("juegos.html")
 app.run('0.0.0.0',5000, debug=True)
